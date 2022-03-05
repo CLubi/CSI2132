347b0d15fe6a86c412d651b03fd0c9b3c6a2ca3c
CLubi <77649649+CLubi@users.noreply.github.com>
2022-03-05 15:23:55 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,48 +1,117 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(DBI)
+library(odbc)
+library(shiny)
+library(shinyjs)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+ui <- dashboardPage(
+    dashboardHeader(title = "Candev"),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Authentification",tabName = "aut", icon = icon("trophy",lib = 'font-awesome')),
+            menuItemOutput("apt"),
+            menuItemOutput("inv"),
+            menuItemOutput("ins"),
+            menuItemOutput("pat")
         )
+    ),
+    dashboardBody(
+        tabItems(
+            tabItem("aut",
+                    fluidPage(
+                        h1("Authentification"),
+                        textInput(inputId = 'userName', label = 'User Name: ', value = "", width = NULL, placeholder = NULL),
+                        passwordInput(inputId = 'password', label = 'Password: ', value = "", width = NULL, placeholder = NULL),
+                        textOutput("invalidLogin"),
+                        actionButton("loginAction","Login",icon = icon("sync-alt",lib = 'font-awesome'))
+                    )
+            ),
+            tabItem('apt',
+                    fluidPage(
+                        h1("Appointments")
+                        )
+                    ),
+            tabItem('inv',
+                    fluidPage(
+                        h1("Invoices")
+                    )
+            ),
+            tabItem('ins',
+                    fluidPage(
+                        h1("Insurance Claims")
+                    )
+            ),
+            tabItem('pat',
+                    fluidPage(
+                        h1("Patients")
+                    )
+            )
+        )
+        
     )
+    
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    #here write query to get a data frame of named 'users' of users
+    user_id = c('user','employee')
+    password = c('userPass','employeePass')
+    users = cbind(user_id,password)
+    users = as.data.frame(users)
+    
+    
+    rv <- reactiveValues()
+    rv <- reactiveValues(Authenticated=FALSE)
+    
+    #Observing the click of the login button
+    observeEvent(input$loginAction,{
+        #Not null user name and password
+        if( is.null(input$userName) | is.null(input$password) ){
+            output$invalidLogin <- renderText({ 'Invalid Login' })
+        }
+        # User name is not valid of password is incorrect
+        else if( !(input$userName %in% users$user_id) | 
+            input$password != toString(users[which(users$user_id == input$userName),which(colnames(users) == 'password')])
+            ){
+            output$invalidLogin <- renderText({ 'Invalid Login' })
+        } # else the user has been authenticated
+        else{
+            rv$Authenticated = TRUE
+            output$invalidLogin <- renderText({ 'Successful authentification' })
+        }
     })
+    
+    #Observing the click of the login button
+    observeEvent(input$loginAction,{
+        
+        #If authenticated then show the various tabs
+        if (rv$Authenticated == TRUE) {
+            
+            output$apt <- renderMenu({
+                menuItem("Appointments", tabName = 'apt', icon = icon("money-bill-alt", lib = 'font-awesome'))
+            })
+            
+            output$inv <- renderMenu({
+                menuItem("Invoices",tabName = "inv", icon = icon("lightbulb",lib = 'font-awesome'))
+            })
+            
+            output$ins <- renderMenu({
+                menuItem("Insurance Claims",tabName = "ins", icon = icon("lightbulb",lib = 'font-awesome'))
+            })
+            
+            output$pat <- renderMenu({
+                menuItem("Patients",tabName = "pat", icon = icon("lightbulb",lib = 'font-awesome'))
+            })
+            
+        }
+        
+    })
+    
 }
 
 # Run the application 
